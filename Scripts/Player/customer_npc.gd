ea71d0e673926b6759ea4693ac28292475ea14b9
babyelaphant <amimutted@gmail.com>
2026-07-01 18:31:31 -0400

    rough draft on how npc behaves

--- Scripts/Player/customer_npc.gd
+++ Scripts/Player/customer_npc.gd
@@ -3,16 +3,31 @@ extends CharacterBody3D
 @export var speed: float = 2.0
 @export var path_parent: Node3D
 
+var start_dialogue: Array = [
+	"hello, mister goat",
+	"where is the diet coke at gangy?",
+	"i been trynna find it",
+]
+
+var has_drink_dialog: Array = [
+	"thanks for the drink",
+]
+
+var current_dialog = 0
 var waypoints: Array = []
 var current_target_index: int = 0
 var is_waiting: bool = false
 var is_walking: bool = false
+var dynamic_label := Label.new()
+var has_drink = false
+@onready var player: CharacterBody3D = $"../Player"
 
 enum State {
 	WALKING,
 	BROWSING,
 	WALKING_TO_REGISTER,
 	AT_REGISTER,
+	WAITING,
 	LEAVING
 }
 
@@ -20,14 +35,17 @@ var current_state: State = State.WALKING
 
 func _ready():
 	randomize() 
+	print(player)
 	for child in get_parent().get_children():
 		if child.name == "customer_path":
 			for c in child.get_children():
-				print("hey")
 				waypoints.append(c.position)
 
 func _physics_process(delta):
-	print(current_target_index)
+
+	if global_position.distance_to( waypoints[5]) < 0.8 and current_target_index == 5 and current_state != State.WAITING and current_state != State.LEAVING:
+		current_state = State.AT_REGISTER
+	
 	match current_state:
 		State.WALKING:
 			is_walking = true
@@ -37,12 +55,47 @@ func _physics_process(delta):
 		State.WALKING_TO_REGISTER:
 			walk_to_register()
 		State.AT_REGISTER:
-			#ready for some dialouge
-			pass
+			if has_drink:
+				start_interaction(has_drink_dialog)
+			else:
+				start_interaction(start_dialogue)
+		State.WAITING:
+			if Input.is_action_just_pressed("skip_dialog") and player.has_drink:
+				has_drink = true
+				current_state = State.AT_REGISTER
 		State.LEAVING:
-			pass
+			leave_store()
 
+func leave_store():
+	
+	var direction = global_position.direction_to(Vector3.ZERO)
+	velocity = direction * speed
+	move_and_slide()
+
+func start_interaction(d: Array):
+	
+	if current_dialog >= d.size():
+		print("hey")
+		current_dialog = 0
+		dynamic_label.visible = false
+		player.is_in_dialog = false
+		current_state = State.WAITING
+		if has_drink:
+			current_state = State.LEAVING
+		return
+	
+	player.is_in_dialog = true
+	dynamic_label.visible = true
+	
+	dynamic_label.position = Vector2(250, 250)
+	dynamic_label.text = d[current_dialog]
+	add_child(dynamic_label)
+	
+	if Input.is_action_just_pressed("skip_dialog"):
+		current_dialog += 1
+	
 func walk_to_register():
+	
 	is_waiting = true
 	current_target_index = 5
 	
@@ -53,6 +106,7 @@ func walk_to_register():
 	move_and_slide()
 	
 	if global_position.distance_to(target_pos) < 0.8:
+		print("at register")
 		current_state = State.AT_REGISTER
 	
 func walking():
@@ -76,10 +130,10 @@ func walking():
 		if current_target_index > 3:
 			percent = 1.0
 		
-		if randf() < percent:
-			get_prev_point()
-		else:
-			get_next_point()
+		#if randf() < percent:
+			#get_prev_point()
+		#else:
+		get_next_point()
 			
 		if randf() < 0.5:
 			current_state = State.BROWSING
@@ -90,7 +144,9 @@ func rest_at_waypoint():
 	
 	await get_tree().create_timer(2.0).timeout
 	is_waiting = false
-	current_state = State.WALKING
+	
+	if current_state != State.WALKING_TO_REGISTER:
+		current_state = State.WALKING
 
 func get_next_point():
 	
--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -12,15 +12,21 @@ const SPEED = 5.0
 
 var vertical_rotation: float = 0.0
 var held_object: Node3D = null
-
-
+var is_in_dialog = false
+var has_drink = false
+var interactable_label = Label.new()
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
+var in_drink_zone = false
 
 func _ready() -> void:
+	
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	ray_cast_3d.enabled = false  # Disabled by default to save performance
 
 func _unhandled_input(event: InputEvent) -> void:
+	
+	if is_in_dialog:
+		return
 	
 	if event.is_action_pressed("ui_cancel"):
 		if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
@@ -36,6 +42,14 @@ func _unhandled_input(event: InputEvent) -> void:
 		camera_3d.rotation.x = vertical_rotation
 
 func _physics_process(delta: float) -> void:
+	
+	if in_drink_zone and Input.is_action_pressed("hold_interact"):
+		has_drink = true
+	
+	if is_in_dialog:
+		fix_camera()
+		return
+
 	# Add the gravity
 	if not is_on_floor():
 		velocity.y -= gravity * delta
@@ -61,6 +75,11 @@ func _physics_process(delta: float) -> void:
 	if held_object != null:
 		held_object.global_transform.origin = camera_3d.global_transform.origin - camera_3d.global_transform.basis.z * hold_distance
 
+func fix_camera():
+	
+	pass
+	#rotation.y = 90
+
 func handle_interact() -> void:
 	# If already holding an object, drop it
 	if held_object != null:
@@ -85,3 +104,19 @@ func handle_interact() -> void:
 		target.pick_up()
 		
 	ray_cast_3d.enabled = false
+
+
+func _on_drinks_body_entered(body: Node3D) -> void:
+	
+	if body.is_in_group("player"):
+		add_child(interactable_label)
+		interactable_label.position = Vector2(500,550)
+		interactable_label.text = "drinks"
+		interactable_label.visible = true
+		in_drink_zone = true
+
+
+func _on_drinks_body_exited(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		interactable_label.visible = false
+		in_drink_zone = false
